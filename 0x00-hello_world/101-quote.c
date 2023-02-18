84234339351e5fb5fbe8075caf132316e70470e0
lass <myemail@gmail.com>
2023-02-18 10:16:39 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unist.h>
+#include <unistd.h>
 /**
  * main - Program to print standard error
- * Return: 1
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
