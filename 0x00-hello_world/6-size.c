80cd06ee076b6f8f14c9b6fd77c208b5013808ab
lass <myemail@gmail.com>
2023-02-18 10:44:25 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
  * main - Printing sixe of vatious types on the computer
- * Return 0
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 char a;
@@ -10,6 +11,7 @@ int b;
 long int c;
 long long int d;
 float e;
+
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
