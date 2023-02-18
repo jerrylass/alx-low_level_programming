c305eea98e9c49b787a0414f0e1b3add81ccbaef
lass <myemail@gmail.com>
2023-02-17 21:42:57 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print program all numbers
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+		}
+	putchar('\n');
+	return (0);
+}
