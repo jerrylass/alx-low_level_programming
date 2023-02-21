e4b248edaca5805029d56f7ecf67f5d1e3ef4787
lass <myemail@gmail.com>
2023-02-20 19:12:22 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Program that print all numbers
  *
@@ -9,7 +10,7 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 10; a++)
 		putchar(a);
 	putchar('\n');
 	return (0);
